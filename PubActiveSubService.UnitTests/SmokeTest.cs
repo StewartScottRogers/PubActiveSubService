a1162da7cef23477b498031a0f2ee97dbd4c2f61
Stewart Rogers <Stewart.Rogers@celltrust.com>
2019-11-15 17:35:52 -0700

    Rapid Dev.

---
+++ PubActiveSubService.UnitTests/SmokeTest.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PubActiveSubService.UnitTests {
+    [TestClass]
+    public class SmokeTest {
+        [TestMethod]
+        public void TestMethod() {
+        }
+    }
+}

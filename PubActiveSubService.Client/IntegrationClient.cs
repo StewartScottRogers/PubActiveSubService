7f43ee8b9cc24038eae1b38380d03a3bd6638d2f
Rogers, Stewart <Stewart.Rogers@advisorgroup.com>
2019-11-20 15:36:54 -0700

    Rapid Dev.

--- PubActiveSubService.Client/IntegrationClient.cs
+++ PubActiveSubService.Client/IntegrationClient.cs
@@ -1,39 +1,91 @@
-﻿using PubActiveSubService.Models;
+﻿using Newtonsoft.Json;
+
+using PubActiveSubService.Models;
+
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
 
 namespace PubActiveSubService {
     public class IntegrationClient : IIntegrationClient {
-        public IEnumerable<ListedChannel> ListChannels(Uri uri, ChannelSearch channelSearch) {
-            throw new System.NotImplementedException();
-        }
 
-        public string Touch(Uri uri) {
-            throw new System.NotImplementedException();
-        }
+        public string Touch(Uri uri)
+         => PublisherClient.Get(uri);
 
-        public string TouchThrough(Uri uri, string urlTarget) {
-            throw new System.NotImplementedException();
-        }
+        public string TouchThrough(Uri uri, string urlTarget)
+            => PublisherClient.Post(urlTarget, uri);
 
-        public string Publish(Uri uri, PublishPackage publishPackage) {
-            throw new System.NotImplementedException();
-        }
+        public IEnumerable<TracedChannel> TraceChannels(Uri uri, ChannelSearch channelSearch)
+            => PublisherClient.Post<IEnumerable<TracedChannel>, ChannelSearch>(channelSearch, uri);
 
-        public string PublishLoopback(Uri uri, PublishPackage publishPackage) {
-            throw new System.NotImplementedException();
-        }
+        public string Publish(Uri uri, PublishPackage publishPackage)
+            => PublisherClient.Post(publishPackage, uri);
 
-        public void Subscribe(Uri uri, Subscribe subscribe) {
-            throw new System.NotImplementedException();
-        }
+        public string PublishLoopback(Uri uri, PublishPackage publishPackage)
+           => PublisherClient.Post(publishPackage, uri);
 
-        public IEnumerable<TracedChannel> TraceChannels(Uri uri, ChannelSearch channelSearch) {
-            throw new System.NotImplementedException();
-        }
+        public IEnumerable<ListedChannel> ListChannels(Uri uri, ChannelSearch channelSearch)
+           => PublisherClient.Post<IEnumerable<ListedChannel>, ChannelSearch>(channelSearch, uri);
 
-        public void Unsubscribe(Uri uri, Unsubscribe unsubscribe) {
-            throw new System.NotImplementedException();
+        public void Subscribe(Uri uri, Subscribe subscribe)
+            => PublisherClient.Post(subscribe, uri);
+
+        public void Unsubscribe(Uri uri, Unsubscribe unsubscribe)
+            => PublisherClient.Post(unsubscribe, uri);
+
+        #region Private Classes
+        private static class PublisherClient {
+            public static string Get(Uri uri) {
+                var stringBuilder = new StringBuilder();
+                using (var httpClient = new HttpClient()) {
+                    try {
+                        var httpResponseMessage = httpClient.GetAsync(uri.OriginalString).Result;
+                        httpResponseMessage.EnsureSuccessStatusCode();
+                        var responseBody = httpResponseMessage.Content.ReadAsStringAsync().Result;
+                        stringBuilder.AppendLine(responseBody);
+                    } catch (System.Exception exception) {
+                        stringBuilder.AppendLine($"Message :{exception.Message}");
+                    }
+                }
+                return stringBuilder.ToString();
+            }
+
+            public static string Post<P>(P p, params Uri[] uris) where P : class {
+                var stringBuilder = new StringBuilder();
+                foreach (var uri in uris)
+                    using (var httpClient = new HttpClient()) {
+                        try {
+                            var contentStyring = JsonConvert.SerializeObject(p);
+                            var httpContent = new StringContent(contentStyring, Encoding.UTF8, "application/json");
+                            var httpResponseMessage = httpClient.PostAsync(uri.OriginalString, httpContent).Result;
+                            httpResponseMessage.EnsureSuccessStatusCode();
+                            var responseBody = httpResponseMessage.Content.ReadAsStringAsync().Result;
+                            stringBuilder.AppendLine(responseBody);
+                        } catch (System.Exception exception) {
+                            stringBuilder.AppendLine($"Message :{exception.Message}");
+                        }
+                    }
+                return stringBuilder.ToString();
+            }
+
+            public static R Post<R, P>(P p, Uri uri) where P : class {
+                using (var httpClient = new HttpClient()) {
+                    var contentStyring = JsonConvert.SerializeObject(p);
+                    var httpContent = new StringContent(contentStyring, Encoding.UTF8, "application/json");
+                    var httpResponseMessage = httpClient.PostAsync(uri.OriginalString, httpContent).Result;
+                    httpResponseMessage.EnsureSuccessStatusCode();
+                    var responseBody = httpResponseMessage.Content.ReadAsStringAsync().Result;
+                    var r = (R)JsonConvert.DeserializeObject(responseBody);
+                    return r;
+                }
+            }
+
+            public static IEnumerable<R> Post<R, P>(P p, params Uri[] uris) where P : class {
+                foreach (var uri in uris)
+                    yield return Post<R, P>(p, uri);
+            }
         }
+        #endregion
     }
 }

5da352dc347beda3a17ffacf24da8bcad92ad6b2
Rogers, Stewart <Stewart.Rogers@advisorgroup.com>
2019-11-26 09:42:33 -0700

    Rapid Dev.

--- PubActiveSubService.Models.SharedProject/Models/Results.cs
+++ PubActiveSubService.Models.SharedProject/Models/Results.cs
@@ -6,5 +6,7 @@ namespace PubActiveSubService.Models {
     public class Results {
         public string Result = string.Empty;
         public bool Success = false;
+
+        public override string ToString() => $"{nameof(Success)}: {Success}, {nameof(Result)}: {Result}.";
     }
 }
--- PubActiveSubService.Services.SharedProject/Internals/Services/PublisherClient.cs
+++ PubActiveSubService.Services.SharedProject/Internals/Services/PublisherClient.cs
@@ -1,6 +1,9 @@
 ﻿using Newtonsoft.Json;
+
 using PubActiveSubService.Internals.Interfaces;
 using PubActiveSubService.Models;
+
+using System;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net.Http;
@@ -9,34 +12,48 @@ using System.Threading.Tasks;
 
 namespace PubActiveSubService.Internals.Services {
     public class PublisherClient : IPublisherClient {
+
         public Results Get(string url) {
-            using (var httpClient = new HttpClient()) {
-                try {
-                    var httpResponseMessage = httpClient.GetAsync(url).Result;
-                    httpResponseMessage.EnsureSuccessStatusCode();
-                    var responseBody = httpResponseMessage.Content.ReadAsStringAsync().Result;
-                    return new Results() { Result = responseBody, Success = true };
-                } catch (System.Exception exception) {
-                    return new Results() { Result = $"Message :{exception.Message}", Success = false };
-                }
-            }
+            ConfirmVWellFormedUriString(url);
+
+            return GetExternal(url);
         }
 
         public Results[] Post<T>(T t, params string[] urls) where T : class {
-            var results = new Collection<Results>();
+            ConfirmVWellFormedUriString(urls);
 
-            Parallel.ForEach(urls, (url) => {
-                                                var publishResult = Post(t, url);
+            var results = new Collection<Results>();
+            Parallel.ForEach(
+                urls, (Action<string>)(
+                                            (url) => {
+                                                var postResults = PostExternal(t, url);
                                                 lock (results) {
-                                                    results.Add(publishResult);
+                                                    results.Add(postResults);
                                                 }
                                             }
+                                      )
             );
-
             return results.ToArray();
         }
 
         public Results Post<T>(T t, string url) where T : class {
+            ;
+            ConfirmVWellFormedUriString(url);
+
+            return PostExternal(t, url);
+        }
+
+        #region Private Methods
+        private static void ConfirmVWellFormedUriString(params string[] urls) {
+            foreach (var url in urls) {
+                System.Diagnostics.Debug.WriteLine(url);
+                if (!Uri.IsWellFormedUriString(url, UriKind.RelativeOrAbsolute))
+                    throw new Exception($"The Url is not Well Formed '{url}'.");
+            }
+        }
+
+
+        private static Results PostExternal<T>(T t, string url) where T : class {
             using (var httpClient = new HttpClient()) {
                 try {
                     var contentStyring = JsonConvert.SerializeObject(t);
@@ -46,9 +63,27 @@ namespace PubActiveSubService.Internals.Services {
                     var responseBody = httpResponseMessage.Content.ReadAsStringAsync().Result;
                     return new Results() { Result = responseBody, Success = true };
                 } catch (System.Exception exception) {
-                    return new Results() { Result = $"Message :{exception.Message}", Success = false };
+                    var results = new Results() { Result = $"{url}, Message :{exception.Message}", Success = false };
+                    System.Diagnostics.Debug.WriteLine(results.ToString());
+                    return results;
                 }
             }
         }
+
+        private static Results GetExternal(string url) {
+            using (var httpClient = new HttpClient()) {
+                try {
+                    var httpResponseMessage = httpClient.GetAsync(url).Result;
+                    httpResponseMessage.EnsureSuccessStatusCode();
+                    var responseBody = httpResponseMessage.Content.ReadAsStringAsync().Result;
+                    return new Results() { Result = responseBody, Success = true };
+                } catch (System.Exception exception) {
+                    var results = new Results() { Result = $"{url}, Message :{exception.Message}", Success = false };
+                    System.Diagnostics.Debug.WriteLine(results.ToString());
+                    return results;
+                }
+            }
+        }
+        #endregion
     }
 }
